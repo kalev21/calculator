3f74db3082a7a630c117cc7c100dc97cdb8350c3
Kalev21 <pfn21@bk.ru>
2021-03-14 11:03:23 +0300

    Add sub func
    Sub func and operator '-' processing

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 def calc(a, b):
     return a + b
 
-
+def sub_nums(a, b):
+    return a - b
 def main():
     num1 = int(input('Введите число А: '))
     num2 = int(input('Введите число В: '))
     operator = input('Введите операцию: ')
     if operator == '+':
         print(calc(num1, num2))
+    elif operator == '-':
+        print(sub_nums(num1, num2))
 
 if __name__ == '__main__':
     main()
