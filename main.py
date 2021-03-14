2286cbbc54c542517faccaa0f82f68eb8e328145
Kalev21 <pfn21@bk.ru>
2021-03-14 11:10:35 +0300

    Add mul and div
    Sub mul and div '*' '/' operator's processing

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ def calc(a, b):
 
 def sub_nums(a, b):
     return a - b
+
+def mul_nums(a, b):
+    return a * b
+
+def div_nums(a, b):
+    return a / b
+
 def main():
     num1 = int(input('Введите число А: '))
     num2 = int(input('Введите число В: '))
@@ -11,6 +18,11 @@ def main():
         print(calc(num1, num2))
     elif operator == '-':
         print(sub_nums(num1, num2))
+    elif operator == '*':
+        print(mul_nums(num1, num2))
+    elif operator == '/':
+        print(div_nums(num1, num2))
+
 
 if __name__ == '__main__':
     main()
